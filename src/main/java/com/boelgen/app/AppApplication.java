a2f438bc029eff447d439a4309a0896da10fa683
Geargrind <lass347s@stud.kea.dk>
2025-05-02 11:33:09 +0200

    gitignore.

--- src/main/java/com/boelgen/app/AppApplication.java
+++ src/main/java/com/boelgen/app/AppApplication.java
@@ -9,6 +9,17 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class AppApplication {
 
 	public static void main(String[] args) {
+         // Load the .env file
+         Dotenv dotenv = Dotenv.configure()
+         .filename("variables.env") // Specify the .env file name
+         .load();
+
+     // Set environment variables
+     System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+     System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+     System.setProperty("DB_HOSTNAME", dotenv.get("DB_HOSTNAME"));
+     System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+     System.setProperty("DB_SSL_CA", dotenv.get("DB_SSL_CA"));
 		SpringApplication.run(AppApplication.class, args);
 	}
 }
