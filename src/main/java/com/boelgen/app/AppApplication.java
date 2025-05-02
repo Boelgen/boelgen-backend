cf7e2a617dabeceafc7f638b38fde6a0356109d9
Geargrind <lass347s@stud.kea.dk>
2025-05-02 12:08:01 +0200

    Managed to get database functionality, with full connectivity.

--- src/main/java/com/boelgen/app/AppApplication.java
+++ src/main/java/com/boelgen/app/AppApplication.java
@@ -11,15 +11,37 @@ public class AppApplication {
 	public static void main(String[] args) {
          // Load the .env file
          Dotenv dotenv = Dotenv.configure()
+        
          .filename("variables.env") // Specify the .env file name
          .load();
 
      // Set environment variables
-     System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-     System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-     System.setProperty("DB_HOSTNAME", dotenv.get("DB_HOSTNAME"));
-     System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-     System.setProperty("DB_SSL_CA", dotenv.get("DB_SSL_CA"));
+     // Set environment variables with null checks
+     String dbUsername = dotenv.get("DB_USERNAME");
+     if (dbUsername != null) {
+         System.setProperty("DB_USERNAME", dbUsername);
+     }
+
+     String dbPassword = dotenv.get("DB_PASSWORD");
+     if (dbPassword != null) {
+         System.setProperty("DB_PASSWORD", dbPassword);
+     }
+
+     String dbHostname = dotenv.get("DB_HOSTNAME");
+     if (dbHostname != null) {
+         System.setProperty("DB_HOSTNAME", dbHostname);
+     }
+
+     String dbName = dotenv.get("DB_NAME");
+     if (dbName != null) {
+         System.setProperty("DB_NAME", dbName);
+     }
+
+     String dbSslCa = dotenv.get("DB_SSL_CA");
+     if (dbSslCa != null) {
+         System.setProperty("DB_SSL_CA", dbSslCa);
+     }
 		SpringApplication.run(AppApplication.class, args);
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
 	}
 }
