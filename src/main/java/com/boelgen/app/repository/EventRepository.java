5dca11e1de60eeaf9be90a2cd9646d5e526d1490
rarogbennu <123975597+rarogbennu@users.noreply.github.com>
2025-05-07 15:36:01 +0200

    done

--- src/main/java/com/boelgen/app/repository/EventRepository.java
+++ src/main/java/com/boelgen/app/repository/EventRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.boelgen.app.model.Event;
 
 @Repository
 public interface EventRepository extends org.springframework.data.jpa.repository.JpaRepository<com.boelgen.app.model.Event, Integer> {
