3cdfc047e831b9f23b0f20d9717157e81da5997d
Geargrind <lass347s@stud.kea.dk>
2025-05-23 13:56:05 +0200

    Added a getAllContacts method in ContactController.

--- src/main/java/com/boelgen/app/controller/ContactController.java
+++ src/main/java/com/boelgen/app/controller/ContactController.java
@@ -1,7 +1,16 @@
 package com.boelgen.app.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
+import com.boelgen.app.model.Event;
 import com.boelgen.app.service.ContactService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.boelgen.app.model.Contact;
+
 @Controller
 public class ContactController {
   
@@ -9,4 +18,11 @@ public class ContactController {
   public ContactController(ContactService contactService) {
     this.contactService = contactService;
   }
+
+  @GetMapping("/contacts")
+  public ResponseEntity<List<Contact>> getAllContacts() {
+      List<Contact> contacts = contactService.getAllContacts();
+      return ResponseEntity.ok(contacts);
+  }
+
 }
